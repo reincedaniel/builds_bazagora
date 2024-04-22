18f8ea23bad95dea4bf45c6514527f6d3617578c
reincedaniel <reincedaniel@gmail.com>
2024-04-22 04:31:04 +0100

    first commit

---
+++ assets/VCardText-BPohWlj4.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-Bb50-H5M.js";const e=t("v-card-text");export{e as V};
---
+++ assets/VSpacer-tDeIcvi7.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./VAvatar-Bb50-H5M.js";const e=r("v-spacer","div","VSpacer");export{e as V};
