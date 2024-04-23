c998a54b58a67f2304daaa7c965cd2e668e6666d
reincedaniel <reincedaniel@gmail.com>
2024-04-23 14:18:28 +0100

    refactor: ups form

---
+++ assets/VCardText-Dh6hPKjL.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-B2DSE7rw.js";const e=t("v-card-text");export{e as V};
---
+++ assets/VNodeRenderer-BW_pD6DH.js
@@ -0,0 +1,1 @@
+import{J as r}from"./index-C9eeWs_S.js";const n=r({name:"VNodeRenderer",props:{nodes:{type:[Array,Object],required:!0}},setup(e){return()=>e.nodes}});export{n as V};
