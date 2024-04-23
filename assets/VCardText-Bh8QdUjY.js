55c47f657386fced243d75fa88bbb64c1679861f
reincedaniel <reincedaniel@gmail.com>
2024-04-23 14:58:00 +0100

    refactor: form netlify

---
+++ assets/VCardText-Bh8QdUjY.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-BQN7R0Ih.js";const e=t("v-card-text");export{e as V};
---
+++ assets/VSpacer-FZj_wccb.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./VAvatar-BQN7R0Ih.js";const e=r("v-spacer","div","VSpacer");export{e as V};
