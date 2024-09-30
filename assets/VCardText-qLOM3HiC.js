4765b0c610e912678f87c8848270400e5c315ee6
reincedaniel <reincedaniel@gmail.com>
2024-09-30 16:36:21 +0100

    feat: v2 bazagora

---
+++ assets/VCardText-qLOM3HiC.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-DkqYST9Q.js";const e=t("v-card-text");export{e as V};
---
+++ assets/VSpacer-Dd05VIhx.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./VAvatar-DkqYST9Q.js";const e=r("v-spacer","div","VSpacer");export{e as V};
